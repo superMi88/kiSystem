af550a93b166c790d34d6c5d31eb2ab8cbccdffd
ble.rai <ble.rai@berufsschule.paulinenpflege.de>
2026-06-17 01:50:28 +0200

    try to create webapp

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'gemini-light-v1';
+const ASSETS = [
+  '/',
+  '/#chat',
+  '/index.html',
+  '/icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).catch(err => console.error("Service worker install cache error:", err))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
