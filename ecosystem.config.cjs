dccdbd1c442914f5748f31faddd0a8baef67e57b
ble.rai <ble.rai@berufsschule.paulinenpflege.de>
2026-09-06 08:27:43 +0200

    chore: update ecosystem config to use fork execution mode instead of instances count

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "kisystem",
       script: "./build/index.js",
-      instances: 1,
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
